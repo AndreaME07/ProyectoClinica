2f9d4e4519c13f6841be7fa9262cf95bcc087fc8
Andrea Medina 07 <andreaaliciamedinaeverardo@gmail.com>
2024-06-29 00:44:56 -0600

    avance del login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, session, flash, url_for,logging
+from flask import Flask, render_template, redirect, request, session, flash, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__, template_folder='template')
@@ -15,33 +15,35 @@ def home():
 
 @app.route('/admin')
 def admin():
-    if 'logueado' in session:
-        return render_template('admin.html')
-    else:
-        flash('Por favor, inicie sesión primero.', 'error')
-        return redirect(url_for('home'))
+    return render_template('admin.html')
 
+# Función de login
 @app.route('/accesoLogin', methods=["GET", "POST"])
-def Login():
-    if request.method == 'POST' and 'txtRFC' in request.form and 'txtPassword' in request.form:
-        frfc = request.form['txtRFC']
-        fpassword = request.form['txtPassword']
-
-        cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM t_login WHERE RFC = %s AND Password = %s', (frfc, fpassword))
-        account = cursor.fetchone()
+def accesoLogin():
+    if request.method == 'POST' and 'txtrfc' in request.form and 'txtpassword' in request.form:
+        frfc = request.form['txtrfc']
+        fpassword = request.form['txtpassword']
+        
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM t_login WHERE rfc = %s AND password = %s', (frfc, fpassword))
+        account = cur.fetchone()
 
         if account:
             session['logueado'] = True
-            session['id'] = account['id']
-            flash('Inicio de sesión exitoso', 'success')
+            session['id'] = account[0]  # Usa el índice adecuado según tu base de datos
             return redirect(url_for('admin'))
         else:
-            flash('Usuario o contraseña incorrectos', 'error')
+            flash("RFC o contraseña incorrecta, revisa tus datos", "danger")
             return redirect(url_for('home'))
-    else:
-        flash('Por favor ingrese sus credenciales', 'error')
-        return redirect(url_for('home'))
+
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/menuAdmin')
+def menuAdm():
+    return render_template('menuadmin.html')
 
 @app.errorhandler(404)
 def paginano(e):
